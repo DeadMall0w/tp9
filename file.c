25437d59e6136ee302768aa619acf48a924b66f3
Raphael Berquier <raphael.berquier@gmail.com>
2025-04-01 17:33:55 +0200

    WORKING

--- file.c
+++ file.c
@@ -3,13 +3,13 @@
 
 
 void debug(char *c){
-    printf("[DEBUG] %s \n", c);
+    // printf("[DEBUG] %s \n", c);
 }
 
 
 void initFile(T_File *ptrF) //mettre Tete et Queue à -1
 {
-    ptrF->Queue = 0;
+    ptrF->Queue = -1;
     ptrF->Tete = 0;
 
     debug("Initialiser de la file réussie !");
@@ -22,9 +22,15 @@ int  retirer(T_File *ptrF, T_Elt *ptrE) //si pas vide, en tete de file
         debug("File vide !");
         return 1;
     }
-
     *ptrE = ptrF->Elts[ptrF->Tete];
-    ptrF->Tete += 1;
+    if (ptrF->Tete == ptrF->Queue){ // On enlève le dernier element, dcp on peut réinitialiser l'élément 
+        initFile(ptrF);
+    }else{   
+        ptrF->Tete += 1;
+        ptrF->Tete = ptrF->Tete % MAX;
+    }
+        
+
     return 0;
 };
 
@@ -32,42 +38,75 @@ int ajouter(T_File *ptrF,T_Elt *ptrE) // si espace libre, ajout en queue
 {
     // printf("queue : %d \n", ptrF->Queue);
     if (filePleine(ptrF)){
-        debug("File pleine !");
+        // printf("File pleine !");
         return 1;
     }
-    affecterElt(&ptrF->Elts[ptrF->Queue], ptrE);
+
     ptrF->Queue += 1;
+    ptrF->Queue = ptrF->Queue % MAX;
+    // printf("POSTION QUEUE : %d \n", ptrF->Queue);
+
+    affecterElt(&ptrF->Elts[ptrF->Queue], ptrE);
     debug("Ajout d'un élément dans la pile !");
     return 0;
 } 
 
 int fileVide(const  T_File *prtF) // qd Tete == 0 
 {
-    return (prtF->Tete == prtF->Queue)?1:0;
+    // return (prtF->Tete == prtF->Queue)?1:0;
+    if (prtF->Queue == -1){
+        return 1;
+    }
+
+    return 0;
 }
 
 int filePleine(const  T_File *ptrF) // qd MAX elts dans la file 
 {
-    if (ptrF->Tete == 0 && ptrF->Queue == MAX){ // file pleine cas 1
+    if (ptrF->Tete == 0 && ptrF->Queue == MAX-1){ // file pleine cas 1
         return 1;
     }
 
-    // if (ptrF->Queue == ptrF->Tete +1){
-    //     return 1;
-    // }
+    if (ptrF->Queue+1 == ptrF->Tete && ptrF->Queue != -1){
+        debug("TETE ET QUEUE TROP PROCHE, DONC FILE PLEINE ! \n");
+        return 1;
+    }
 
     return 0;
-    // return 0;
 }
 
 T_Elt  premier(T_File *ptrF) //valeur en tete de file
 {
-return 0;
+    if (fileVide(ptrF)){
+        // return 0;
+        printf("Pile vide ! \n");
+    }
+
+    return ptrF->Elts[ptrF->Tete];
 }
 
 void afficherFile(T_File *ptrF)
 {
+    T_Elt e;
 
+    T_File emp;
+    initFile(&emp);
+    // Dans un premier temps nous allons copier tous les éléments dans une autre file
+    while (fileVide(ptrF) == 0){ // Tant que la file n'est pas vide
+        retirer(ptrF, &e);
+        // debugFile(ptrF);
+        afficherElt(&e);
+        // printf("\n");
+        ajouter(&emp, &e);
+    
+    }
+
+    while (fileVide(&emp) == 0){ // Tant que la file n'est pas vide
+        retirer(&emp, &e);
+        // afficherElt(&e);
+        // printf("\n");
+        ajouter(ptrF, &e);
+    }
 }
 
 // Affiche la pile comme un tableau surtout utiliser pour le débogage 
@@ -75,7 +114,7 @@ void debugFile(T_File *ptrF){
     // debug("debug FILE !");
     printf("tete %d & queue %d \n", ptrF->Tete, ptrF->Queue);
     int i;
-    for (i = ptrF->Tete; i < ptrF->Queue; i++)
+    for (i = ptrF->Tete; i < ptrF->Queue+1; i++)
     {
         printf("index %d : ", i);
 
--- tp9.c
+++ tp9.c
@@ -13,6 +13,17 @@ printf("\n\n\n 1 : tester mon fichier file.c");
 printf("\n 2 : tester mon fichier pile.c");
 printf("\n 3 : afficher et compter les permutations d'une chaine");
 printf("\n 4 : afficher et compter les solutions pour un échiquier ");
+printf("\n 20 : File - Initialisation ");
+printf("\n 21 : File - Ajouter ");
+printf("\n 22 : File - Retirer ");
+printf("\n 23 : File - Vide ");
+printf("\n 24 : File - Pleine ");
+printf("\n 25 : File - Affichage ");
+
+
+
+// printf("\n 4 : afficher et compter les solutions pour un échiquier ");
+// printf("\n 4 : afficher et compter les solutions pour un échiquier ");
 printf("\n 0 :  QUITTER  ");
 printf("\n votre choix ?  ");
 scanf("%d",&choix);
@@ -24,6 +35,7 @@ int main()
 T_File mafile;
 T_Pile mapile;
 int chx;
+int res;
 // int taille;
 //char chaine[20];
 
@@ -38,6 +50,127 @@ switch (chx)
 		break;
 	case 2 : //testez toutes vos fonctions par un jeu de test de votre choix
 		// testFile(&mafile); //TP9 partie 1 : à ecrire 
+
+		printf("Intialisation de la file \n");
+		initFile(&mafile);
+		printf("Test 1 - Voir si la file est pleine/vide\n");
+		res = fileVide(&mafile);
+		if (res == 0)
+			printf("File non vide\n");
+		else
+			printf("File vide\n");
+		
+		printf("\n");
+
+		res = filePleine(&mafile);
+		if (res == 0)
+			printf("File non pleine\n");
+		else
+			printf("File pleine\n");
+
+		printf("Test 2 - Ajout de 10 d'éléments\n");
+		for  (int i = 0; i < 10; i++)
+		{
+			T_Elt t = i;
+			printf("Ajout de %d \n", t);
+			res = ajouter(&mafile, &t);
+
+			if (res == 0)
+				printf("Ajout réussi !\n");
+			else
+				printf("Erreur : file pleine \n");
+		}
+
+		printf("Test 3 - Voir si la file est pleine/vide\n");
+		res = fileVide(&mafile);
+		if (res == 0)
+			printf("File non vide\n");
+		else
+			printf("File vide\n");
+		
+		printf("\n");
+
+		res = filePleine(&mafile);
+		if (res == 0)
+			printf("File non pleine\n");
+		else
+			printf("File pleine\n");
+
+
+
+		printf("Affichage de la file : \n");
+		afficherFile(&mafile);
+
+		printf("Test 4 - Tenter d'enlever et afficher 10 éléments\n");
+		for  (int i = 0; i < 10; i++)
+		{
+			T_Elt t;
+			res = retirer(&mafile, &t);
+
+			if (res ==0)
+				afficherElt(&t);
+			else 
+				printf("Erreur : file vide\n");
+		}
+
+		printf("Test 5 - Ajout et suppression d'éléments\n");
+
+		printf("Ajout de 17\n");
+		T_Elt t = 17;
+		res = ajouter(&mafile, &t);
+		
+		printf("Ajout de 18\n");
+		t = 18;
+		res = ajouter(&mafile, &t);
+
+		printf("Ajout de 19\n");
+		t = 19;
+		res = ajouter(&mafile, &t);
+		
+		printf("Affichage du premier élément\n");
+
+		T_Elt te;
+		res = retirer(&mafile, &te);
+		afficherElt(&te);
+
+
+		printf("Affichage du premier élément\n");
+		te;
+		res = retirer(&mafile, &te);
+		afficherElt(&te);
+
+		
+		printf("Ajout de 20\n");
+		t = 20;
+		res = ajouter(&mafile, &t);
+		
+		printf("\nAffichage du premier élément\n");
+		te;
+		res = retirer(&mafile, &te);
+		afficherElt(&te);
+
+		
+		
+		printf("Ajout de 21\n");
+		t = 21;
+		res = ajouter(&mafile, &t);
+		
+		printf("\nAffichage du premier élément\n");
+		te;
+		res = retirer(&mafile, &te);
+		afficherElt(&te);
+
+		
+		printf("\nAffichage du premier élément\n");
+		te;
+		res = retirer(&mafile, &te);
+		afficherElt(&te);
+
+		
+
+
+
+
 		break; 
 	case 3 : 
 		//scanf("%s",chaine); //une chaine de longueur <=MAX
@@ -50,47 +183,64 @@ switch (chx)
 	case 20:
 		//* Initialiser la pile
 		initFile(&mafile);
-		// T_Elt un = 1;
-		// T_Elt trois = 3;
-		// T_Elt cinq = 5;
-
-		// ajouter(&mafile, &un);
-		// ajouter(&mafile, &trois);
-		// ajouter(&mafile, &cinq);
-		for (int i = 0; i < 3; i++)
-		{
-			T_Elt e;
-			printf("Entrer une valeur à ajouter : ");
-			saisirElt(&e);
-			ajouter(&mafile, &e);
-			debugFile(&mafile);	
-		}
 		
-
-		// TODO : code de bebug
 		break;
 	case 21:
 		//* Ajouter un element da la pile
 		T_Elt e;
 		printf("Entrer une valeur à ajouter : ");
 		saisirElt(&e);
-		ajouter(&mafile, &e);
+		res = ajouter(&mafile, &e);
+		if (res == 0)
+			printf("Ajout réussi !\n");
+		else
+			printf("Erreur : file pleine \n");
 		break;
 	case 22:
 		//* Défiler un élément
-		T_Elt te;
-		int result = retirer(&mafile, &te);
-		if (result ==0)
-			afficherElt(&te);
+		T_Elt tet;
+		res = retirer(&mafile, &tet);
+		if (res ==0)
+			afficherElt(&tet);
+		else 
+			printf("Erreur : file vide\n");
 		break;
 	case 23:
 		//* Renvoie 1 ou 0 si la file est vide ou pas
-		printf("Etat de remplissage de la pile : %d \n", fileVide(&mafile));
+		res = fileVide(&mafile);
+		if (res == 0)
+			printf("File non vide\n");
+		else
+			printf("File vide\n");
+		// printf("Etat de remplissage de la pile : %d \n", fileVide(&mafile));
 		break;
 	case 24:
-		printf("File pleine ? %d \n", filePleine(&mafile));
+		res = filePleine(&mafile);
+		if (res == 0)
+			printf("File non pleine\n");
+		else
+			printf("File pleine\n");
 		
 		break;
+	case 25:
+		printf("Affichage de la file : \n");
+		afficherFile(&mafile);
+		break;
+	case 26:
+		//* AJouter 3 éléments dans la pile
+		for (int i = 0; i < 3; i++)
+		{
+			T_Elt e;
+			printf("Entrer une valeur à ajouter : ");
+			saisirElt(&e);
+			ajouter(&mafile, &e);
+		}
+	case 27:
+		// T_Elt e;  
+		T_Elt ett = premier(&mafile); //valeur en tete de file
+		afficherElt(&ett);
+		break;		
+
 	case 29:
 		debugFile(&mafile);
 		break;
